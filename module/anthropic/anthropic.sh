5f1d29cd427ea69a627c860c9363220e645a8803
zengweitao <zengweitao@gmail.com>
2025-08-09 08:26:52 +0800

    init

---
+++ module/anthropic/anthropic.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# author : zengweitao@gmail.com
+# datetime : 2024/05/20
+
+:<<'MARK'
+# https://docs.github.com/en/code-security/secret-scanning/working-with-secret-scanning-and-push-protection/working-with-push-protection-from-the-command-line#removing-a-secret-introduced-by-an-earlier-commit-on-your-branch
+working with push protection from the command line ：
+0，git log
+1，git rebase -i {$commitId}~1
+2，[delete commit] dd && :wq
+3，[check conflict] git status
+4，[resolve conflict] git add module/confict.file && git rebase --continue
+5，git push origin main
+MARK
+
+declare -A VARI_GLOBAL
+VARI_GLOBAL["BUILTIN_BASH_EVNI"]="SLAVE"
+VARI_GLOBAL["BUILTIN_UNIT_ROOT_PATH"]=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+VARI_GLOBAL["BUILTIN_UNIT_FILENAME"]=$(basename "$(readlink -f "${BASH_SOURCE[0]}")")
+source "${VARI_GLOBAL["BUILTIN_UNIT_ROOT_PATH"]}/../../include/builtin/builtin.sh"
+source "${VARI_GLOBAL["BUILTIN_UNIT_ROOT_PATH"]}/../../include/utility/utility.sh"
+source "${VARI_GLOBAL["BUILTIN_UNIT_ROOT_PATH"]}/encrypt.envi" 2> /dev/null || true
+
+# ##################################################
+# reset builtin variable[START]
+
+# reset builtin variable[END]
+# ##################################################
+
+# ##################################################
+# global variable[START]
+
+# global variable[END]
+# ##################################################
+
+# ##################################################
+# protected function[START]
+
+# protected function[END]
+# ##################################################
+
+# ##################################################
+# public function[START]
+function funcPublicClaudeCodeReinit(){
+  # 構建鏡像[START]
+  variParameterDescList=("image pattern（example ：chunio/go:1.22.4）")
+  funcProtectedCheckOptionParameter 1 variParameterDescList[@] $# || return ${VARI_GLOBAL["BUILTIN_SUCCESS_CODE"]}
+  variImagePattern=${1-"chunio/go:1.22.4"}
+  variContainerName="go1224Environment"
+  docker builder prune --all -f
+  docker rm -f ${variContainerName} 2> /dev/null
+  docker rmi -f $variImagePattern 2> /dev/null
+  # 鏡像不存在時自動執行：docker pull $variImageName
+  docker run -d --privileged --name ${variContainerName} -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /windows:/windows --tmpfs /run --tmpfs /run/lock -p 80:80 centos:7.9.2009 /sbin/init
+  docker exec -it ${variContainerName} /bin/bash -c "cd ${VARI_GLOBAL["BUILTIN_UNIT_ROOT_PATH"]} && ./$(basename "${VARI_GLOBAL["BUILTIN_UNIT_FILENAME"]}") 1224EnvironmentInit;"
+  variContainerId=$(docker ps --filter "name=${variContainerName}" --format "{{.ID}}")
+  echo "docker commit $variContainerId $variImagePattern"
+  docker commit $variContainerId $variImagePattern
+  docker ps --filter "name=${variContainerName}"
+  docker images --filter "reference=${variImagePattern}"
+  echo "${FUNCNAME} ${VARI_GLOBAL["BUILTIN_TRUE_LABEL"]}" >> ${VARI_GLOBAL["BUILTIN_UNIT_TRACE_URI"]}
+  echo "docker exec -it ${variContainerName} /bin/bash" >> ${VARI_GLOBAL["BUILTIN_UNIT_TRACE_URI"]}
+  return 0
+}
+# public function[END]
+# ##################################################
+
+source "${VARI_GLOBAL["BUILTIN_UNIT_ROOT_PATH"]}/../../include/workflow/workflow.sh"
